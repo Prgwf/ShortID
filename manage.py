007c34745093bf75d3aeaf5eec94844d33ffa0b9
Prgwf <peter.wfgu@gmail.com>
2018-04-11 09:41:13 +0800

    bug: redirect...

--- manage.py
+++ manage.py
@@ -1,17 +1,17 @@
 # -*- coding: utf-8 -*-
-from flask import Flask, redirect, url_for, session, flash, render_template
+from flask import Flask, redirect, url_for, session, flash, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_restful import Api, Resource
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, validators
-
+import re
 from convert import Convert
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:gwf@localhost:3306/ShortID_db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = 'true'
-app.config['SECRET_KEY'] = 'dont do badthings to me please~'
+app.config['SECRET_KEY'] = 'dont do badthings to me please'
 db = SQLAlchemy(app)
 api = Api(app)
 bootstrap = Bootstrap(app)
@@ -48,19 +48,36 @@ def internal_server_error():
 @app.route('/', methods=['GET', 'POST'])
 def index():
     form = URLForm()
-    if form.validate_on_submit():
-        session['url'] = form.url.data
-        info = RawURL.query.filter_by(url=form.url.data).first()
-        # graceful byebye~
-        if info is None:
-            info = RawURL(url=form.url.data)
-            db.session.add(info)
-            db.session.commit()
+    if request.method == 'GET':
+        return render_template('index.html', form=form)
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            session['url'] = form.url.data
+            info = RawURL.query.filter_by(url=form.url.data).first()
+            # graceful byebye~
+            if info is None:
+                info = RawURL(url=form.url.data)
+                db.session.add(info)
+                db.session.commit()
 
-        session['s_url'] = radix_transfer.toStr(info.uid)
-        # todo 'front end|architecture|RESTful'
-        return render_template('index.html', form=form, url=session.get('url'), s_url=session.get('s_url'))
-    return render_template('index.html', form=form)
+            session['s_url'] = radix_transfer.toStr(info.uid)
+            # todo 'front end|architecture|RESTful'
+            print(url_for('index', _external=True) + session.get('s_url'))
+            return render_template('index.html', form=form, url=session.get('url'), s_url=session.get('s_url'))
+        return render_template('index.html', form=form)
+
+@app.route('/<string:str>')
+def let_s_jump(str):
+    r = url_for('let_s_jump', _external=True, str=str)
+    print(r)
+    uid = radix_transfer.toNum(str)
+    line = RawURL.query.filter_by(uid=uid).first()
+    if line is None:
+        page_not_found()
+        return
+    # if line.url.find('http://') or line.url.find('https://')
+    return redirect(line.url)
+
 
 
 if __name__ == '__main__':
